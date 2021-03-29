ed61321deda40c0a11d4603b2d823e28630d9283
Ilya Senchukov <senchukov02@gmail.com>
2021-03-29 17:50:27 +0300

    Fix

---
+++ Sources/NetworkManagement/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  
+//
+//  Created by Ilya Senchukov on 28.03.2021.
+//
+
+import Foundation
+
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case delete = "DELETE"
+    case patch = "PATCH"
+    case put = "PUT"
+}
